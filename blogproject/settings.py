2596b429d6c72c995e5d5bbb3561ea5f0ec3ce64
林炜翔 <handsomelinweixiang@gmail.com>
2017-06-13 20:48:59 +0800

    change static

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+     os.path.join(BASE_DIR, 'blog/static').replace('\\', '/'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
